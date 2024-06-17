4b23faf30f3aa317b2bdc3f360c111c3b38f3bc1
hfxalbees <huifangwork@gmail.com>
2024-06-18 02:28:15 +0800

    Update to base template

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 # Define the path to your file
 file_path = 'CVE data/CVE_Data_2023.xlsx'  # Replace with your actual file path
 
+@app.route('/') 
+def index(): 
+    return render_template('index.html') 
 
-@app.route('/')
+@app.route('/2023')
 def view():
     try:
         # Read the file using pandas
@@ -17,6 +20,28 @@ def view():
         return data.to_html()
     except Exception as e:
         return f'Error reading file: {str(e)}'
+    
+@app.route('/example')
+def homepage():
+ 
+    # Define Plot Data 
+    labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+    ]
+ 
+    data = [0, 10, 15, 8, 22, 18, 25]
+ 
+    # Return the components to the HTML template 
+    return render_template(
+        template_name_or_list='chartjs-example.html',
+        data=data,
+        labels=labels,
+    )
 
 
 if __name__ == '__main__':
