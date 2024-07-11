071b22029e5228eec12158fdb439dc11e2e943cd
WZC-SIT <145113472+WZC-SIT@users.noreply.github.com>
2024-07-11 22:12:23 +0800

    Levenshtein Distance
    This folder includes the following:
    • Text file containing 100 most common words
    • Implementation of the Levenshtein Distance algorithm
    
    Usage:
    • Specify the file path to the text file
    • Input a string to test for typo
    • If a typo is present, similar words will be recommended
    • If there is no typo, it will return the string entered
    
    Known Limitations:
    • Does not detect a typo if no similar words are found in the text file.

---
+++ Levenshtein Distance/Levenshtein Distance Implementation.py
@@ -0,0 +1,51 @@
+import numpy
+
+dictionary_path = "100_common_words.txt"
+
+with open(dictionary_path, "r") as dictionary:
+    dictionary_data = dictionary.read().splitlines()
+
+
+def levenshtein(token1, token2):
+    distance = numpy.zeros((len(token1) + 1, len(token2) + 1))
+
+    for t1 in range(len(token1) + 1):
+        distance[t1][0] = t1
+
+    for t2 in range(len(token2) + 1):
+        distance[0][t2] = t2
+
+    for t1 in range(1, len(token1) + 1):
+        for t2 in range(1, len(token2) + 1):
+            if token1[t1 - 1] == token2[t2 - 1]:
+                distance[t1][t2] = distance[t1 - 1][t2 - 1]
+            else:
+                insert_cost = distance[t1][t2 - 1]
+                delete_cost = distance[t1 - 1][t2]
+                replace_cost = distance[t1 - 1][t2 - 1]
+
+                if insert_cost <= delete_cost and insert_cost <= replace_cost:
+                    distance[t1][t2] = insert_cost + 1
+                elif delete_cost <= insert_cost and delete_cost <= replace_cost:
+                    distance[t1][t2] = delete_cost + 1
+                else:
+                    distance[t1][t2] = replace_cost + 1
+
+    return distance[len(token1)][len(token2)]
+
+
+user_input = input("\nInput a string: ")
+closely_matched_word_list = []
+
+for data in dictionary_data:
+    if levenshtein(user_input, data) == 0:
+        break
+    elif levenshtein(user_input, data) < 2:
+        closely_matched_word_list.append(data)
+
+if len(closely_matched_word_list) == 0:
+    print("\nYou have entered the following string:", user_input)
+else:
+    print("\nIt looks like there is a typo in your input.")
+    print("Did you mean any of the following?")
+    print(closely_matched_word_list)
